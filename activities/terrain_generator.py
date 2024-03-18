834ec24c8ad216bef9f53272127b126396caadfc
qasimAde <qaadegbi@ncsu.edu>
2024-03-18 13:42:48 -0400

    adding new tangible files

--- activities/terrain_generator.py
+++ activities/terrain_generator.py
@@ -16,13 +16,25 @@ import grass.script as gs
 def generate_terrain(rows, cols, env):
     # Generate synthetic elevation data
     elevation = np.random.rand(rows, cols) * 1000
-    np.savetxt('elevation.txt', elevation, fmt='%f')
+    np.savetxt("elevation.txt", elevation, fmt="%f")
 
     # Import elevation data into GRASS GIS
-    gs.run_command("r.in.xyz", input="elevation.txt", output="synthetic_elevation", separator="comma", flags="o", env=env)
+    gs.run_command(
+        "r.in.xyz",
+        input="elevation.txt",
+        output="synthetic_elevation",
+        separator="comma",
+        flags="o",
+        env=env,
+    )
 
     # Compute slope using GRASS GIS
-    gs.run_command("r.slope.aspect", elevation="synthetic_elevation", slope="synthetic_slope", env=env)
+    gs.run_command(
+        "r.slope.aspect",
+        elevation="synthetic_elevation",
+        slope="synthetic_slope",
+        env=env,
+    )
 
 
 def main():
