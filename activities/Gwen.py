d01bc851e0feba420cb023a033ca8de285306a82
gkirschke <151567170+gkirschke@users.noreply.github.com>
2024-03-19 12:50:21 -0400

    formatting, removing residual code from example

--- activities/Gwen.py
+++ activities/Gwen.py
@@ -6,7 +6,27 @@ import grass.script as gs
 
 import numpy as np
 
-#change detection from analyses.py
+
+def get_environment(**kwargs):
+    """!Returns environment for running modules.
+    All modules for which region is important should
+    pass this environment into run_command or similar.
+    @param tmp_regions a list of temporary regions
+    @param kwargs arguments for g.region
+    @return environment as a dictionary
+    """
+    env = os.environ.copy()
+    env["GRASS_OVERWRITE"] = "1"
+    env["GRASS_VERBOSE"] = "0"
+    env["GRASS_MESSAGE_FORMAT"] = "standard"
+    region3d = False
+    if "raster_3d" in kwargs:
+        region3d = True
+    env["GRASS_REGION"] = gs.region_env(region3d=region3d, **kwargs)
+    return env
+
+
+# change detection from analyses.py
 def change_detection(
     before,
     after,
@@ -20,10 +40,10 @@ def change_detection(
 ):
     diff_thr = "diff_thr_" + str(uuid.uuid4()).replace("-", "")
     diff_thr_clump = "diff_thr_clump_" + str(uuid.uuid4()).replace("-", "")
-    coeff = gcore.parse_command(
+    coeff = gs.parse_command(
         "r.regression.line", mapx=after, mapy=before, flags="g", env=env
     )
-    grast.mapcalc(
+    gs.mapcalc(
         "diff = {a} + {b} * {after} - {before}".format(
             a=coeff["a"], b=coeff["b"], before=before, after=after
         ),
@@ -31,7 +51,7 @@ def change_detection(
     )
     try:
         if add:
-            grast.mapcalc(
+            gs.mapcalc(
                 "{diff_thr} = if(({a} + {b} * {after} - {before}) > {thr1} &&"
                 " ({a} + {b} * {after} - {before}) < {thr2}, 1, null())".format(
                     a=coeff["a"],
@@ -45,7 +65,7 @@ def change_detection(
                 env=env,
             )
         else:
-            grast.mapcalc(
+            gs.mapcalc(
                 "{diff_thr} = if(({before} - {a} + {b} * {after}) > {thr}, 1, null())".format(
                     diff_thr=diff_thr,
                     a=coeff["a"],
@@ -57,10 +77,16 @@ def change_detection(
                 env=env,
             )
 
-        gcore.run_command("r.clump", input=diff_thr, output=diff_thr_clump, env=env)
+        gs.run_command(
+            "r.clump", input=diff_thr, output=diff_thr_clump, env=env
+        )
         stats = (
-            gcore.read_command(
-                "r.stats", flags="cn", input=diff_thr_clump, sort="desc", env=env
+            gs.read_command(
+                "r.stats",
+                flags="cn",
+                input=diff_thr_clump,
+                sort="desc",
+                env=env,
             )
             .strip()
             .splitlines()
@@ -82,7 +108,7 @@ def change_detection(
                     cats.append(cat)
             if cats:
                 rules = ["{c}:{c}:1".format(c=c) for c in cats]
-                gcore.write_command(
+                gs.write_command(
                     "r.recode",
                     input=diff_thr_clump,
                     output=change,
@@ -90,7 +116,7 @@ def change_detection(
                     stdin="\n".join(rules),
                     env=env,
                 )
-                gcore.run_command(
+                gs.run_command(
                     "r.volume",
                     flags="f",
                     input=change,
@@ -99,13 +125,13 @@ def change_detection(
                     env=env,
                 )
             else:
-                gcore.warning("No change found!")
-                gcore.run_command("v.edit", map=change, tool="create", env=env)
+                gs.warning("No change found!")
+                gs.run_command("v.edit", map=change, tool="create", env=env)
         else:
-            gcore.warning("No change found!")
-            gcore.run_command("v.edit", map=change, tool="create", env=env)
+            gs.warning("No change found!")
+            gs.run_command("v.edit", map=change, tool="create", env=env)
 
-        gcore.run_command(
+        gs.run_command(
             "g.remove",
             flags="f",
             type=["raster"],
@@ -113,7 +139,7 @@ def change_detection(
             env=env,
         )
     except:
-        gcore.run_command(
+        gs.run_command(
             "g.remove",
             flags="f",
             type=["raster"],
@@ -121,27 +147,50 @@ def change_detection(
             env=env,
         )
 
+
 def run_distance(real_elev, scanned_elev, env, **kwargs):
-    env2 = get_environment(raster=scanned_elev, n='n-100', s='s+100', e='e-100', w='w+100') # not sure what shifting this does?   
-    before = 'scan_saved' #shouldn't this be real_elev?
-    
+    # not sure what shifting this does?
+    env2 = get_environment(
+        raster=scanned_elev, n="n-100", s="s+100", e="e-100", w="w+100"
+    )
+    before = "scan_saved"
+
     # get new garden location points from pins
-    change_detection(before=before, after=scanned_elev,
-                              change='change', height_threshold=[15, 100], cells_threshold=[5, 100], add=True, max_detected=13, debug=True, env=env2)
+    change_detection(
+        before=before,
+        after=scanned_elev,
+        change="change",
+        height_threshold=[15, 100],
+        cells_threshold=[5, 100],
+        add=True,
+        max_detected=13,
+        debug=True,
+        env=env2,
+    )
     # dict of new points
     points = {}
-    data = gs.read_command('v.out.ascii', input='trail_points', type='point', format='point', env=env).strip() # where does trail_points come from?
-    c1, c2 = data.splitlines()
-    c1 = c1.split('|')
-    c2 = c2.split('|')
-    points[0] = (float(c1[0]), float(c1[1]))
-    points[1] = (float(c2[0]), float(c2[1]))
+    gs.run_command("v.edit", tool="create", map=trail, env=env)
+    # detected points
+    points_raw = (
+        gs.read_command(
+            "v.out.ascii", input="change", type="point", format="point"
+        )
+        .strip()
+        .split()
+    )
+    i = 0
+    for point in points_raw:
+        point = point.split("|")
+        point = (float(point[0]), float(point[1]))
+        points[i] = point
+        i += 1
 
     # calculate shortest distance between points (replace this with least cost path later)
-    gs.read_command("v.net.allpairs", input=data, output=dist_matrix) #data is the map version of the points, right?
+    dist_matrix = gs.read_command("v.net.allpairs", input=trail)
     shortest_dist = np.matrix.min(dist_matrix)
 
     # display the length of this shortest distance
+    return shortest_dist
 
 
 def main():
@@ -149,9 +198,10 @@ def main():
     env["GRASS_OVERWRITE"] = "1"
     elevation = "elev_lid792_1m"
     elev_resampled = "elev_resampled"
-    #garden_locations = "" #how do I include this file?
     gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
-    gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
+    gs.run_command(
+        "r.resamp.stats", input=elevation, output=elev_resampled, env=env
+    )
 
     run_distance(real_elev=elevation, scanned_elev=elev_resampled, env=env)
 
