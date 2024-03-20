bd7cef2fe05cbdbfa5ac2b5fd3cd3e99b6f811a1
coflemin <coflemin@ncsu.edu>
2024-03-20 11:55:59 -0400

    Removed commented code

--- activities/Fleming.py
+++ activities/Fleming.py
@@ -24,10 +24,6 @@ def run_create_hog_lagoon(env):
     )
 
 
-# def run_slope(scanned_elev, env, **kwargs):
-#     gs.run_command("r.slope.aspect", elevation=scanned_elev, slope="slope", env=env)
-
-
 def run_lake(scanned_elev, env, **kwargs):
     coordinates = "638830,220150"
     gs.run_command(
@@ -38,12 +34,6 @@ def run_lake(scanned_elev, env, **kwargs):
         water_level=120,
         env=env,
     )
-
-
-# def run_check_flood_impact(env):
-#     gs.run_command("v.to.rast", input="hog_lagoon", output="hog_lagoon_raster", use="val", value=1, env=env)
-#     gs.mapcalc("flood_impact = if(hog_lagoon_raster > 0 && output_lake > 0, 1)", env=env)
-#     gs.run_command("r.colors", map="flood_impact", color="rules", rules="-", stdin="1 brown", env=env)
 
 
 def main():
@@ -57,7 +47,6 @@ def main():
     )
     run_lake(scanned_elev=elev_resampled, env=env)
     run_create_hog_lagoon(env)
-    # run_check_flood_impact(env)
 
 
 if __name__ == "__main__":
