47ad0097bfc549018e7cd2b3d450d4fd6ae4c58e
qasimAde <61097452+qasimAde@users.noreply.github.com>
2024-03-20 14:32:29 -0400

    Add files via upload

--- activities/terrain_generator2.py
+++ activities/terrain_generator2.py
@@ -13,19 +13,33 @@ import grass.script as gs
 
 def generate_terrain(env):
     # Get the number of rows and columns from the current computational region
-    region_info = gs.read_command('g.region', flags='g', stdout=gs.PIPE, env=env)
-    rows = int(region_info.split('\n')[0].split('=')[1])
-    cols = int(region_info.split('\n')[1].split('=')[1])
+    region_info = gs.read_command(
+        "g.region", flags="g", stdout=gs.PIPE, env=env
+    )
+    rows = int(region_info.split("\n")[0].split("=")[1])
+    cols = int(region_info.split("\n")[1].split("=")[1])
 
     # Generate synthetic elevation data
     elevation = np.random.rand(rows, cols) * 1000
-    np.savetxt('elevation.txt', elevation, fmt='%f')
+    np.savetxt("elevation.txt", elevation, fmt="%f")
 
     # Import elevation data into GRASS GIS
-    gs.run_command("r.in.xyz", input="elevation.txt", output="synthetic_elevation", separator="comma", flags="o", env=env)
+    gs.run_command(
+        "r.in.xyz",
+        input="elevation.txt",
+        output="synthetic_elevation",
+        separator="comma",
+        flags="o",
+        env=env,
+    )
 
     # Compute slope using GRASS GIS
-    gs.run_command("r.slope.aspect", elevation="synthetic_elevation", slope="synthetic_slope", env=env)
+    gs.run_command(
+        "r.slope.aspect",
+        elevation="synthetic_elevation",
+        slope="synthetic_slope",
+        env=env,
+    )
 
 
 def main():
