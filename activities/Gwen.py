74b400634befd189f301d4a8101af651bd13592e
gkirschke <151567170+gkirschke@users.noreply.github.com>
2024-04-11 10:19:32 -0400

    changed code used for calculations; added dashboard

--- activities/Gwen.py
+++ activities/Gwen.py
@@ -1,209 +1,101 @@
 #!/usr/bin/env python3
 
+"""
+Instructions
+
+- Functions intended to run for each scan
+  need to start with `run_`, e.g., `run_slope`.
+
+- Do not modify the parameters of the `run_` function
+  unless you know what you are doing.
+  See optional parameters at:
+  https://github.com/tangible-landscape/grass-tangible-landscape/wiki/Running-analyses-and-developing-workflows#python-workflows
+
+- All gs.run_command/read_command/write_command/parse_command
+  need to be passed *env* parameter like this: `(..., env=env)`.
+"""
+
 import os
 
 import grass.script as gs
 
-import numpy as np
 
+def run_function_with_points(scanned_elev, env, points=None, **kwargs):
+    """Doesn't do anything, except loading points from a vector map to Python
 
-def get_environment(**kwargs):
-    """!Returns environment for running modules.
-    All modules for which region is important should
-    pass this environment into run_command or similar.
-    @param tmp_regions a list of temporary regions
-    @param kwargs arguments for g.region
-    @return environment as a dictionary
+    If *points* is provided, the function assumes it is name of an existing vector map.
+    This is used during testing.
+    If *points* is not provided, the function assumes it runs in Tangible Landscape.
     """
-    env = os.environ.copy()
-    env["GRASS_OVERWRITE"] = "1"
-    env["GRASS_VERBOSE"] = "0"
-    env["GRASS_MESSAGE_FORMAT"] = "standard"
-    region3d = False
-    if "raster_3d" in kwargs:
-        region3d = True
-    env["GRASS_REGION"] = gs.region_env(region3d=region3d, **kwargs)
-    return env
+    if not points:
+        # If there are no points, ask Tangible Landscape to generate points from
+        # a change in the surface.
+        points = "points"
+        import analyses
 
-
-# change detection from analyses.py
-def change_detection(
-    before,
-    after,
-    change,
-    height_threshold,
-    cells_threshold,
-    add,
-    max_detected,
-    debug,
-    env,
-):
-    diff_thr = "diff_thr_" + str(uuid.uuid4()).replace("-", "")
-    diff_thr_clump = "diff_thr_clump_" + str(uuid.uuid4()).replace("-", "")
-    coeff = gs.parse_command(
-        "r.regression.line", mapx=after, mapy=before, flags="g", env=env
-    )
-    gs.mapcalc(
-        "diff = {a} + {b} * {after} - {before}".format(
-            a=coeff["a"], b=coeff["b"], before=before, after=after
-        ),
-        env=env,
-    )
-    try:
-        if add:
-            gs.mapcalc(
-                "{diff_thr} = if(({a} + {b} * {after} - {before}) > {thr1} &&"
-                " ({a} + {b} * {after} - {before}) < {thr2}, 1, null())".format(
-                    a=coeff["a"],
-                    b=coeff["b"],
-                    diff_thr=diff_thr,
-                    after=after,
-                    before=before,
-                    thr1=height_threshold[0],
-                    thr2=height_threshold[1],
-                ),
-                env=env,
-            )
-        else:
-            gs.mapcalc(
-                "{diff_thr} = if(({before} - {a} + {b} * {after}) > {thr}, 1, null())".format(
-                    diff_thr=diff_thr,
-                    a=coeff["a"],
-                    b=coeff["b"],
-                    after=after,
-                    before=before,
-                    thr=height_threshold,
-                ),
-                env=env,
-            )
-
-        gs.run_command(
-            "r.clump", input=diff_thr, output=diff_thr_clump, env=env
-        )
-        stats = (
-            gs.read_command(
-                "r.stats",
-                flags="cn",
-                input=diff_thr_clump,
-                sort="desc",
-                env=env,
-            )
-            .strip()
-            .splitlines()
-        )
-        if debug:
-            print("DEBUG: {}".format(stats))
-        if len(stats) > 0 and stats[0]:
-            cats = []
-            found = 0
-            for stat in stats:
-                if found >= max_detected:
-                    break
-                if (
-                    float(stat.split()[1]) < cells_threshold[1]
-                    and float(stat.split()[1]) > cells_threshold[0]
-                ):  # larger than specified number of cells
-                    found += 1
-                    cat, value = stat.split()
-                    cats.append(cat)
-            if cats:
-                rules = ["{c}:{c}:1".format(c=c) for c in cats]
-                gs.write_command(
-                    "r.recode",
-                    input=diff_thr_clump,
-                    output=change,
-                    rules="-",
-                    stdin="\n".join(rules),
-                    env=env,
-                )
-                gs.run_command(
-                    "r.volume",
-                    flags="f",
-                    input=change,
-                    clump=diff_thr_clump,
-                    centroids=change,
-                    env=env,
-                )
-            else:
-                gs.warning("No change found!")
-                gs.run_command("v.edit", map=change, tool="create", env=env)
-        else:
-            gs.warning("No change found!")
-            gs.run_command("v.edit", map=change, tool="create", env=env)
-
-        gs.run_command(
-            "g.remove",
-            flags="f",
-            type=["raster"],
-            name=[diff_thr, diff_thr_clump],
-            env=env,
-        )
-    except:
-        gs.run_command(
-            "g.remove",
-            flags="f",
-            type=["raster"],
-            name=[diff_thr, diff_thr_clump],
+        analyses.change_detection(
+            "scan_saved",
+            scanned_elev,
+            points,
+            height_threshold=[10, 100],
+            cells_threshold=[5, 50],
+            add=True,
+            max_detected=5,
+            debug=True,
             env=env,
         )
 
+    # generate random point for focal garden
+    gs.run_command("v.random", output=focal_garden, npoints=1, seed=3)
+    
+    # calculate the cost surface
+    gs.run_command("r.cost", input="cfactorgrow_1m", output=cost_surface, start_points=focal_garden, stop_points=points)
+    
+    #calculate connectivity metric
+    data = gs.read_command("r.what", map=cost_surface, points=points)
+    data_lines = data.splitlines()
+    data = data_lines.split('|')
+    i = 3
+    connectivity = 0 
+    for datum in data:
+        connectivity += 1/data[i]
+        i += 3
 
-def run_distance(real_elev, scanned_elev, env, **kwargs):
-    # not sure what shifting this does?
-    env2 = get_environment(
-        raster=scanned_elev, n="n-100", s="s+100", e="e-100", w="w+100"
-    )
-    before = "scan_saved"
+    # display the connectivity metric
+    event = updateDisplay(value=connectivity) 
+    eventHandler.postEvent(receiver=eventHandler.activities_panel, event=event
 
-    # get new garden location points from pins
-    change_detection(
-        before=before,
-        after=scanned_elev,
-        change="change",
-        height_threshold=[15, 100],
-        cells_threshold=[5, 100],
-        add=True,
-        max_detected=13,
-        debug=True,
-        env=env2,
-    )
-    # dict of new points
-    points = {}
-    gs.run_command("v.edit", tool="create", map=trail, env=env)
-    # detected points
-    points_raw = (
-        gs.read_command(
-            "v.out.ascii", input="change", type="point", format="point"
-        )
-        .strip()
-        .split()
-    )
-    i = 0
-    for point in points_raw:
-        point = point.split("|")
-        point = (float(point[0]), float(point[1]))
-        points[i] = point
-        i += 1
-
-    # calculate shortest distance between points (replace this with least cost path later)
-    dist_matrix = gs.read_command("v.net.allpairs", input=trail)
-    shortest_dist = np.matrix.min(dist_matrix)
-
-    # display the length of this shortest distance
-    return shortest_dist
 
 
 def main():
+    """Function which runs when testing without Tangible Landscape"""
+
+    # No need to edit this block. It should stay the same.
+    # Get the current environment variables as a copy.
     env = os.environ.copy()
+    # We want to run this repetitively and replace the old data by the new data.
     env["GRASS_OVERWRITE"] = "1"
     elevation = "elev_lid792_1m"
     elev_resampled = "elev_resampled"
+    # We use resampling to get a similar resolution as with Tangible Landscape.
     gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
-    gs.run_command(
-        "r.resamp.stats", input=elevation, output=elev_resampled, env=env
-    )
+    gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
+    # The end of the block which needs no editing.
 
-    run_distance(real_elev=elevation, scanned_elev=elev_resampled, env=env)
+    # Code specific to testing of the analytical function.
+    # Create points which is the additional input needed for the process.
+    points = "points"
+    gs.write_command(
+        "v.in.ascii",
+        flags="t",
+        input="-",
+        output=points,
+        separator="comma",
+        stdin="638432,220382\n638621,220607",
+        env=env,
+    )
+    # Call the analysis.
+    run_function_with_points(scanned_elev=elev_resampled, env=env, points=points)
 
 
 if __name__ == "__main__":
