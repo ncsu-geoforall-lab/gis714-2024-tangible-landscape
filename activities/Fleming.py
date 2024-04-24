a61b36835810d6198323ea8d2f99fb2ef3b3b55a
coflemin <coflemin@ncsu.edu>
2024-04-24 09:32:19 -0400

    removed lagoon to simplify activity

--- activities/Fleming.py
+++ activities/Fleming.py
@@ -4,28 +4,8 @@ import os
 import grass.script as gs
 
 
-def run_create_hog_lagoon(env):
-    lagoon_centroid = "638550,220550"
-    gs.write_command(
-        "v.in.ascii",
-        input="-",
-        output="hog_lagoon_centroid",
-        stdin=lagoon_centroid,
-        format="point",
-        separator=",",
-        env=env,
-    )
-    gs.run_command(
-        "v.buffer",
-        input="hog_lagoon_centroid",
-        output="hog_lagoon",
-        distance=50,
-        env=env,
-    )
-
-
 def run_lake(scanned_elev, env, **kwargs):
-    coordinates = "638830,220150"
+    coordinates = [638830, 220150]
     gs.run_command(
         "r.lake",
         elevation=scanned_elev,
@@ -46,7 +26,6 @@ def main():
         "r.resamp.stats", input=elevation, output=elev_resampled, env=env
     )
     run_lake(scanned_elev=elev_resampled, env=env)
-    run_create_hog_lagoon(env)
 
 
 if __name__ == "__main__":
